5cdaa85f4d38787b13814237737c5f88bfbfc775
Sumit Samanta <sumit.samanta@jungleworks.co>
2018-04-26 16:27:15 +0530

    find users api

--- server.js
+++ server.js
@@ -3,10 +3,18 @@ var nodemailer = require('nodemailer');
 var app = express();
 var fs = require("fs");
 var mongoose = require('mongoose');
+var bodyParser = require("body-parser");
+var db;
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.use(bodyParser.json());
 
 mongoose.connect('mongodb://127.0.0.1:27017/users')
     .then((mongoose) => {
-        mongoose = mongoose;
+        db = mongoose;
         console.log("Successfully connected to the database");
     })
     .catch(err => {
@@ -42,6 +50,31 @@ app.get('/listUsers', function (req, res) {
         });
 })
 
+app.post('/addUser', function (req, res) {
+    var document = mongoose.models.User(req.body);
+    mongoose.models.User.db.collection("users").insertOne(document, function (error, response) {
+        if (err) {
+            res.status(500).send({
+                message: err.message || "Some error occurred while inserting user."
+            });
+            throw err;
+        }
+        res.status(201).send('1 document inserted');
+    });
+})
+
+app.post('/findUsers', function (req, res) {
+    var query = { name: req.body.name };
+    mongoose.models.User.find(query)
+        .then(users => {
+            res.status(201).send(users);
+        }).catch(err => {
+            res.status(500).send({
+                message: err.message || "Some error occurred while retrieving users."
+            });
+        });
+})
+
 var server = app.listen(8081, function () {
 
     var host = server.address().address;
@@ -51,20 +84,20 @@ var server = app.listen(8081, function () {
 
 })
 
-var transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-        user: 'youremail@gmail.com',
-        pass: 'yourpassword'
-    }
-});
+// var transporter = nodemailer.createTransport({
+//     service: 'gmail',
+//     auth: {
+//         user: 'youremail@gmail.com',
+//         pass: 'yourpassword'
+//     }
+// });
 
-var mailOptions = {
-    from: 'youremail@gmail.com',
-    to: 'sender@gmail.com',
-    subject: 'Sending Email using Node.js',
-    text: 'That was easy!'
-};
+// var mailOptions = {
+//     from: 'youremail@gmail.com',
+//     to: 'sender@gmail.com',
+//     subject: 'Sending Email using Node.js',
+//     text: 'That was easy!'
+// };
 
 // transporter.sendMail(mailOptions, function (error, info) {
 //     if (error) {
